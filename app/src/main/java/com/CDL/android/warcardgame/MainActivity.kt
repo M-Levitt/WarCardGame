c1d01c66d1c19205e99486d7a1fb35f9393ecd6c
Matthew <matthewlevitt20@gmail.com>
2021-04-19 22:21:17 -0700

    Initial commit

---
+++ app/src/main/java/com/CDL/android/warcardgame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.CDL.android.warcardgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
